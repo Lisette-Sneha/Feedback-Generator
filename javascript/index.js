45a983f9297a43c361bb04810245caf4a578927a
Lisette Venema <lisettevenema@hotmail.com>
2020-08-01 16:19:06 +0200

    show examples toggle added to time

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,19 @@ const situation = document.getElementById('situation')
 const others = document.getElementById('others')
 const buildButton = document.getElementById('build-button')
 const finalFeedback = document.getElementById('final-feedback')
-buildButton.addEventListener('click', () => {
-  finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value)
-test()
+const timeExamplesBtn = document.getElementById('time-examples-btn')
+timeExamplesBtn.addEventListener('click', () => {
+    toggleSection()
 })
+buildButton.addEventListener('click', () => {
+  finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value)  
+})
+
+function toggleSection () {
+    const x = document.getElementById("time-examples-section");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
