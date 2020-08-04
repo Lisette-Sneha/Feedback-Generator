b438da953ce998520c29bee96aaf85ada1b90069
Lisette Venema <lisettevenema@hotmail.com>
2020-08-04 21:26:17 +0200

    added goodWords and badWords filter but score does not work yet

--- javascript/feedback.js
+++ javascript/feedback.js
@@ -1,9 +1,45 @@
-function test () {
-    console.log('Guido rocks today')
+// function describeContext (time, situation, location, others) {
+// return `${time} ${situation} ${location} ${others}`
+// }
+let totalScore = 50
+const badWords = ['shitty', 'dumb', 'retarded']
+const badWordsFromUser = []
+const wordsOfEncouragement =['impressive', 'incredible', 'inspiring','brave']
+const goodWordsFromUser = []
+
+const checkForBadWords = (array) => {
+    array.forEach(word => {
+    console.log(word)
+    if (badWords.includes(word.toLowerCase())) {
+        alert ("Hey you, don't say such bad words! Use respectful language")
+          badWordsFromUser.push(word)
+            totalScore -= 10
+    }
+})
+console.log(totalScore)
 }
 
-function describeContext (time, situation, location, others) {
-return `${time} ${situation} ${location} ${others}`
+const checkForWordsOfEncouragement = (array) => {
+    array.forEach(word => {
+    if (wordsOfEncouragement.includes(word.toLowerCase())) {
+      alert("Hey, we really like the way you are writing your feedback with words of encouragement")
+      goodWordsFromUser.push(word)
+        }
+    })
+}
+
+const describeContext = (...whateverTheyType) => {
+// // whateverTheyType.forEach(word => {
+// // if (badWords.includes(word.toLowerCase())) {
+// //     alert ("Hey you, don't say such bad words! Use respectful language")
+// //       badWordsFromUser.push(word)
+        
+    
+// }
+//   })
+checkForBadWords(whateverTheyType)
+checkForWordsOfEncouragement(whateverTheyType)
+return whateverTheyType.join(' ')
 }
 
 function toggleSection (sectionId) {
--- javascript/index.js
+++ javascript/index.js
@@ -7,12 +7,23 @@ const buildButton = document.getElementById('build-button')
 const finalFeedback = document.getElementById('final-feedback')
 const timeExamplesBtn = document.getElementById('time-examples-btn')
 const situationExamplesBtn = document.getElementById('situation-examples-btn')
+const locationExamplesBtn = document.getElementById('location-examples-btn')
+const othersExamplesBtn = document.getElementById('others-examples-btn')
+
 timeExamplesBtn.addEventListener('click', () => {
   toggleSection('time-examples-section')
 })
 situationExamplesBtn.addEventListener('click', () => {
   toggleSection('situation-examples-section')
 })
+locationExamplesBtn.addEventListener('click', () => {
+  toggleSection('location-examples-section')
+})
+othersExamplesBtn.addEventListener('click', () => {
+  toggleSection('others-examples-section')
+})
 buildButton.addEventListener('click', () => {
   finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value)  
 })
+
+
