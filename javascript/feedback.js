5a4b8339c18fd997944aad256b613d89dc485a8e
Lisette Venema <lisettevenema@hotmail.com>
2020-08-04 19:13:33 +0200

    to make sure we have the code saved

--- javascript/feedback.js
+++ javascript/feedback.js
@@ -5,3 +5,12 @@ function test () {
 function describeContext (time, situation, location, others) {
 return `${time} ${situation} ${location} ${others}`
 }
+
+function toggleSection (sectionId) {
+    const x = document.getElementById(sectionId)
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
--- javascript/index.js
+++ javascript/index.js
@@ -6,18 +6,13 @@ const others = document.getElementById('others')
 const buildButton = document.getElementById('build-button')
 const finalFeedback = document.getElementById('final-feedback')
 const timeExamplesBtn = document.getElementById('time-examples-btn')
+const situationExamplesBtn = document.getElementById('situation-examples-btn')
 timeExamplesBtn.addEventListener('click', () => {
-    toggleSection()
+  toggleSection('time-examples-section')
+})
+situationExamplesBtn.addEventListener('click', () => {
+  toggleSection('situation-examples-section')
 })
 buildButton.addEventListener('click', () => {
   finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value)  
 })
-
-function toggleSection () {
-    const x = document.getElementById("time-examples-section");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
