e76349391e6bb8b2a0b06b6eabd58ef65a19c2e0
Lisette Venema <lisettevenema@hotmail.com>
2020-08-06 21:50:11 +0200

    Mobile first design 80% done

--- javascript/feedback.js
+++ javascript/feedback.js
@@ -1,31 +1,37 @@
 // function describeContext (time, situation, location, others) {
-// return `${time} ${situation} ${location} ${others}`
+// return `${time} ${situation} ${location} ${others} ${observation} ${impact} ${next}`
 // }
-let totalScore = 50
-const badWords = ['shitty', 'dumb', 'retarded']
+let totalScore = 60
+const badWords = ['shitty', 'dumb', 'retarded', 'stupid', 'lazy', 'horrible']
 const badWordsFromUser = []
-const wordsOfEncouragement =['impressive', 'incredible', 'inspiring','brave']
+const wordsOfEncouragement =['impressive', 'incredible', 'inspiring','brave', 'impactful', 'amazing']
 const goodWordsFromUser = []
 
 const checkForBadWords = (array) => {
-    array.forEach(word => {
-    console.log(word)
-    if (badWords.includes(word.toLowerCase())) {
-        alert ("Hey you, don't say such bad words! Use respectful language")
-          badWordsFromUser.push(word)
-            totalScore -= 10
-    }
-})
-console.log(totalScore)
+  array.forEach(word => {
+    badWords.forEach(badWord => {
+      if (word.includes(badWord)) {
+        alert("Hey you, don't say such bad words! Use respectful language")
+        badWordsFromUser.push(word)
+        totalScore -= 10
+      }
+    })
+  })
+  console.log(totalScore)
 }
 
 const checkForWordsOfEncouragement = (array) => {
     array.forEach(word => {
-    if (wordsOfEncouragement.includes(word.toLowerCase())) {
-      alert("Hey, we really like the way you are writing your feedback with words of encouragement")
-      goodWordsFromUser.push(word)
+      wordsOfEncouragement.forEach(goodWord => {
+        if (word.includes(goodWord)) {
+          alert("Hey, we really like the way you are writing your feedback with words of encouragement")
+          goodWordsFromUser.push(word)
+          totalScore += 10
         }
+      }
+    )    
     })
+  console.log(totalScore)
 }
 
 const describeContext = (...whateverTheyType) => {
--- javascript/index.js
+++ javascript/index.js
@@ -3,12 +3,18 @@ const time = document.getElementById('time')
 const myLocation = document.getElementById('location')
 const situation = document.getElementById('situation')
 const others = document.getElementById('others')
+const observation = document.getElementById('observation')
+const impact = document.getElementById('impact')
+const next = document.getElementById('next')
 const buildButton = document.getElementById('build-button')
 const finalFeedback = document.getElementById('final-feedback')
 const timeExamplesBtn = document.getElementById('time-examples-btn')
 const situationExamplesBtn = document.getElementById('situation-examples-btn')
 const locationExamplesBtn = document.getElementById('location-examples-btn')
 const othersExamplesBtn = document.getElementById('others-examples-btn')
+const observationExamplesBtn = document.getElementById('observation-examples-btn')
+const impactExamplesBtn = document.getElementById('impact-examples-btn')
+const nextExamplesBtn = document.getElementById('next-examples-btn')
 
 timeExamplesBtn.addEventListener('click', () => {
   toggleSection('time-examples-section')
@@ -22,8 +28,17 @@ locationExamplesBtn.addEventListener('click', () => {
 othersExamplesBtn.addEventListener('click', () => {
   toggleSection('others-examples-section')
 })
+observationExamplesBtn.addEventListener('click', () => {
+  toggleSection('observation-examples-section')
+})
+impactExamplesBtn.addEventListener('click', () => {
+  toggleSection('impact-examples-section')
+})
+nextExamplesBtn.addEventListener('click', () => {
+  toggleSection('next-examples-section')
+})
 buildButton.addEventListener('click', () => {
-  finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value)  
+  finalFeedback.innerHTML = describeContext(time.value, situation.value, myLocation.value, others.value, observation.value, impact.value, next.value)  
 })
 
 
